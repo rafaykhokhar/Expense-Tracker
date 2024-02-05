527d2ac23ee44c6326c79c066687fd1c94b70bb4
Muhammad Rafay Shafqat <rafaykhokhar.ra@gmail.com>
2024-02-05 07:43:16 -0800

    first commit

---
+++ expense_tracker/lib/expenses.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({
+    super.key,
+  });
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Groceries',
+        amount: 100.0,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'cenima',
+        amount: 250,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
+        ],
+      ),
+    );
+  }
+}
