5d6bbd8aea20845966f9e583645496c3097e7934
Muhammad Rafay Shafqat <rafaykhokhar.ra@gmail.com>
2024-02-07 03:31:14 -0800

    added modal bottom sheet

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({
@@ -19,16 +20,30 @@ class _ExpensesState extends State<Expenses> {
         date: DateTime.now(),
         category: Category.food),
     Expense(
-        title: 'cenima',
+        title: 'Cinema',
         amount: 250,
         date: DateTime.now(),
         category: Category.leisure),
   ];
+
+  void openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+              onPressed: openAddExpenseOverlay, icon: const Icon(Icons.add)),
+        ],
+      ),
       body: Column(
         children: [
+          const SizedBox(height: 05),
           const Text('chart'),
           Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
--- expense_tracker/lib/widgets/expenses_list/expense_item.dart
+++ expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
-
-
 class ExpenseItem extends StatelessWidget {
-  const ExpenseItem(this.expense,{super.key});
-  
+  const ExpenseItem(this.expense, {super.key});
+
   final Expense expense;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(
+              height: 4,
+            ),
+            // Text('\$${expense.amount.toStringAsFixed(2)}'),
+            Row(
+              children: [
+                Text('Rs ${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(expense.formattedDate)
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
